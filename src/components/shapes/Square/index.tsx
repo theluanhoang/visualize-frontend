aebda85ecec4a96a9b9e6b752aa31a6f4953940c
luantrum27 <hoangtheluan2016@gmail.com>
2024-09-10 14:04:02 +0700

    information block

--- src/components/shapes/Square/index.tsx
+++ src/components/shapes/Square/index.tsx
@@ -1,21 +1,39 @@
+import Konva from "konva";
+import { useEffect, useRef, useState } from "react";
 import { Arrow, Group, Rect, Text } from "react-konva";
 
 type SquareProps = {
   x: number;
   y: number;
-  width: number;
-  height: number;
   branch: string;
 };
 
-const Square = ({ x, y, branch, width, height }: SquareProps) => {
-  const rectWidth = 100;
-  const rectHeight = 50;
+const Square = ({ x, y, branch }: SquareProps) => {
+  const [textSize, setTextSize] = useState({ width: 100, height: 50 });
+  const textRef = useRef<Konva.Text>(null); // Dùng kiểu Konva.Text cho ref
+
+  useEffect(() => {
+    if (textRef.current) {
+      const width = textRef.current.width();
+      const height = textRef.current.height();
+      setTextSize({ width: width , height: height + 10 });
+    }
+  }, [branch]);
+
+  const rectX = 10; // X của Rect
+  const rectY = 0; // Y của Rect
+  const cornerRadius = 10;
+
+  const arrowStartX = rectX + cornerRadius / 2;
+  const arrowStartY = rectY + textSize.height - cornerRadius / 2; // Góc trái dưới của Rect, đã tính cả chiều cao
+
+  const arrowEndX = arrowStartX - 30;
+  const arrowEndY = arrowStartY + 30;
 
   return (
     <Group x={x} y={y}>
       <Arrow
-        points={[30, 30, 0, 70]}
+        points={[arrowStartX, arrowStartY, arrowEndX, arrowEndY]} // Vị trí mũi tên
         fill="#FF66D3"
         stroke="#FF66D3"
         strokeWidth={10}
@@ -24,26 +42,26 @@ const Square = ({ x, y, branch, width, height }: SquareProps) => {
         tension={0}
       />
       <Rect
-        x={10}
-        y={0}
-        width={rectWidth}
-        height={rectHeight}
+        x={rectX}
+        y={rectY}
+        width={textSize.width}
+        height={textSize.height}
         fill="#FF66D3"
         stroke="white"
         strokeWidth={2}
-        cornerRadius={10}
+        cornerRadius={cornerRadius} // Bo tròn góc
       />
-
       <Text
-        x={10}
-        y={0}
-        width={rectWidth}
-        height={rectHeight}
+        ref={textRef} // Dùng ref để lấy kích thước của Text
+        x={rectX} // Căn giữa ngang dựa trên vị trí của Rect
+        y={rectY} // Căn giữa dọc dựa trên vị trí của Rect
+        width={textSize.width} // Đặt chiều rộng của Text bằng với Rect
+        height={textSize.height} // Đặt chiều cao của Text bằng với Rect
         text={branch}
         fontSize={18}
         fill="#000"
-        align="center"
-        verticalAlign="middle"
+        align="center" // Căn giữa ngang
+        verticalAlign="middle" // Căn giữa dọc
       />
     </Group>
   );
